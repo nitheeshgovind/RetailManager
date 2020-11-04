22169ce004d98b0ba20739c116e8edac2320465d
Nitheesh Govind <nitheesh.govind@gmail.com>
2020-11-04 15:26:13 +0530

    DAL Added
    New project DataManager.Library is added to access the datbase using Dapper

---
+++ RMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RMDataManager.Library.Internal.DataAccess;
+using RMDataManager.Library.Models;
+
+namespace RMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var parameter = new { Id = id };
+
+            var users = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", parameter, "RMData");
+
+            return users;
+        }
+    }
+}
---
+++ RMDataManager/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using RMDataManager.Library.DataAccess;
+using RMDataManager.Library.Models;
+
+namespace RMDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public IEnumerable<UserModel> GetById()
+        {
+            string id = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+            var users = data.GetUserById(id);
+            return users;
+        }
+    }
+}
