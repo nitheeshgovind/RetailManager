fb75abc68530e31fc85d49391810143e43bdf60c
Nitheesh Govind <nitheesh.govind@gmail.com>
2020-11-04 14:05:56 +0530

    Login Screen and API Helper Added
    Login screen is added to the application and APIHelper is added to communicate with the API

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Caliburn.Micro;
+using RMDesktopUI.Helpers;
 using RMDesktopUI.ViewModels;
 
 namespace RMDesktopUI
@@ -17,6 +19,11 @@ namespace RMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
 
         protected override void Configure()
@@ -25,7 +32,8 @@ namespace RMDesktopUI
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper>();
 
             GetType().Assembly.GetTypes()
                 .Where(type => type.IsClass)
---
+++ RMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+using RMDesktopUI.Helpers;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private IAPIHelper _apiHelper;
+        private string _userName;
+        private string _password;
+        private string _errorMessage;
+
+        /// <summary>
+        /// Username of the user
+        /// </summary>
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; NotifyOfPropertyChange(() => UserName); NotifyOfPropertyChange(() => CanLogin); }
+        }
+
+        /// <summary>
+        /// Password for the user account
+        /// </summary>
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; NotifyOfPropertyChange(() => Password);  NotifyOfPropertyChange(() => CanLogin); }
+        }
+
+        /// <summary>
+        /// Flag to indicate if there is error in the Login action
+        /// </summary>
+        public bool IsErrorVisible
+        {
+            get { return !string.IsNullOrEmpty(ErrorMessage); }
+        }
+
+        /// <summary>
+        /// Error message of the Login action
+        /// </summary>
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => ErrorMessage);
+                NotifyOfPropertyChange(() => IsErrorVisible);
+            }
+        }
+
+        /// <summary>
+        /// Flag to indentify if the logic action can be started
+        /// </summary>
+        /// <param name="userName">Username provided by the user</param>
+        /// <param name="password">Password provided by the user</param>
+        /// <returns></returns>
+        public bool CanLogin
+        {
+            get
+            {
+                if (UserName?.Length > 0 && Password?.Length > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        public async Task Login()
+        {
+            ErrorMessage = string.Empty;
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch(Exception e)
+            {
+                ErrorMessage = e.Message;
+            }
+        }
+    }
+}
