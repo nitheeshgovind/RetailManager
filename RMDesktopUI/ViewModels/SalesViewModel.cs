43f30bdad46a52aca2d9b7d81bc4ebab3b4d3d80
Nitheesh Govind <nitheesh.govind@gmail.com>
2020-11-04 16:50:21 +0530

    Sales Page Design completed

---
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set { _products = value; NotifyOfPropertyChange(() => Products); }
+        }
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set { _itemQuantity = value; NotifyOfPropertyChange(() => ItemQuantity); }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set { _cart = value; NotifyOfPropertyChange(() => Cart); }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+        public string Tax
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+    }
+}
